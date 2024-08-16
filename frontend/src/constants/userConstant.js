a0c8379646201b001e4b371a3fb3415a95947e22
Rutuja Kodag <kodagrutuja0@gmail.com>
2024-08-16 14:36:43 +0530

    export all constants required for user authentication

--- frontend/src/constants/userConstant.js
+++ frontend/src/constants/userConstant.js
@@ -13,4 +13,4 @@ export const LOAD_USER_FAIL="LOAD_USER_FAIL";
 export const LOGOUT_SUCCESS="LOGOUT_SUCCESS";
 export const LOGOUT_FAIL="LOGOUT_FAIL";
 
-export const CLEAR_ERRORS="CLEAR_ERROR";
+export const CLEAR_ERRORS="CLEAR_ERRORS";
