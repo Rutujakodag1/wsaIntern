22914447a4c13a02a433064c7e53781a9e894723
Rutuja Kodag <kodagrutuja0@gmail.com>
2024-08-16 14:44:26 +0530

    path to the login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Header from "./components/layouts/Header";
 import Home from "./components/layouts/Home";
 import Footer from "./components/layouts/Footer";
 import Menu from "./components/layouts/Menu";
+import Login from "./components/users/Login";
+import Register from "./components/users/Register";
 import Cart from "./components/cart/Cart";
 
 export default function App() {
@@ -17,6 +19,8 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/eats/stores/:id/menus" element={<Menu />}/>
+      <Route path="/users/login" element={<Login />}/>
+      <Route path="/users/signup" element={<Register/>}/>
     </Routes>
    
    </div>
