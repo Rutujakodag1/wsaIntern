99b832eeb9d86dffab7b792833bdb3d6417a15ab
Rutuja Kodag <kodagrutuja0@gmail.com>
2024-08-23 21:34:38 +0530

    Added User Authentication

--- frontend/src/components/users/Register.jsx
+++ frontend/src/components/users/Register.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import useAlert from "react-alert";
+import React, { useEffect, useState } from "react";
+import { useAlert }from "react-alert";
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
+import { clearErrors, register } from "../../actions/userAction";
 
 
 const Register = () => {
@@ -22,14 +23,60 @@ const Register = () => {
     (state)=>state.auth
   );
 
+  //handle redirection with useEffect
+  useEffect(()=>{
+    if (isAuthenticated){
+      navigate("/");
+    }
+    if (error){
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+  },[dispatch,alert,isAuthenticated,error,navigate]);
 
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    if(password !== passwordConfirm){
+      alert.error("password dont matched");
+      return;
+    }
+    const formData=new FormData();
+    formData.set("name",name);
+    formData.set("email",email);
+    formData.set("password",password);
+    formData.set("passwordConfirm",passwordConfirm);
+    formData.set("phoneNumber",phoneNumber);
+    if (avatar===""){
+      formData.set("avatar","/images/images.png");
+    }else{
+      formData.set("avatar",avatar);
+    }
+
+
+    dispatch(register(formData));
+  };
+
+  const onChange=(e)=>{
+    if (e.target.name==="avatar"){
+      const reader=new FileReader();
+      reader.onload=()=>{
+        if (reader.readyState===2){
+          setAvatarPreview(reader.result);
+          setAvatar(reader.result);
+        }
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }else{
+      setUser({...user,[e.target.name]:e.target.value});
+    }
+  };
 
 
   return (
     <>
       <div className="row wrapper">
         <div className="col-10 col-lg-5 registration-form">
-          <form className="shadow-lg" encType="multipart/form-data">
+          <form className="shadow-lg" encType="multipart/form-data" onSubmit={submitHandler}>
             <h1 className="mb-3">Register</h1>
             <div className="form-group">
               <label htmlFor="name_field">Name</label>
@@ -38,7 +85,8 @@ const Register = () => {
                 id="name_field"
                 className="form-control"
                 name="name"
-                value={"YourName"}
+                value={name}
+                onChange={onChange}
               ></input>
             </div>
             <div className="form-group">
@@ -48,7 +96,8 @@ const Register = () => {
                 id="email_field"
                 className="form-control"
                 name="email"
-                value={"abc@email.com"}
+                value={email}
+                onChange={onChange}
               ></input>
             </div>
             <div className="form-group">
@@ -58,7 +107,8 @@ const Register = () => {
                 id="password_field"
                 className="form-control"
                 name="password"
-                value={"12345678"}
+                value={password}
+                onChange={onChange}
               ></input>
             </div>
             <div className="form-group">
@@ -68,7 +118,8 @@ const Register = () => {
                 id="passwordConfirm_field"
                 className="form-control"
                 name="passwordConfirm"
-                value={"12345678"}
+                value={passwordConfirm}
+                onChange={onChange}
               ></input>
             </div>
             <div className="form-group">
@@ -78,7 +129,8 @@ const Register = () => {
                 id="phoneNumber_field"
                 className="form-control"
                 name="phoneNumber"
-                value={"9874563210"}
+                value={phoneNumber}
+                onChange={onChange}
               ></input>
             </div>
             <div className="form-group">
@@ -87,7 +139,7 @@ const Register = () => {
                 <div>
                   <figure className="avatar mr-3 item-rtl">
                     <img
-                      src={""}
+                      src={avatarPreview}
                       className="rounded-circle"
                       alt="Avatar Preview"
                     />
@@ -100,6 +152,7 @@ const Register = () => {
                     className="custom-file-input"
                     id="customFile"
                     accept="images/*"
+                    onChange={onChange}
                   ></input>
                   <label className="custom-file-label" htmlFor="customFile">
                     Choose Avatar
@@ -112,7 +165,7 @@ const Register = () => {
               id="register_button"
               type="submit"
               className="btn btn-block py-3"
-              disabled={5 > 10 ? true : false}
+              disabled={loading? true : false}
             >
               REGISTER
             </button>
